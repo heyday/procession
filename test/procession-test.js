6f69dd5172e017babf6702055d28c12f4cbf6c92
Pieter Vanderwerff <pieter@heyday.co.nz>
2013-04-17 16:08:18 +1200

    Added unload unit tests

--- test/procession-test.js
+++ test/procession-test.js
@@ -17,7 +17,7 @@ define( [ 'buster', '../procession', 'when' ], function( buster, Procession, whe
 			expect( new Procession() ).toBeObject();
 		} );
 
-		describe( 'queue to procession', function() {
+		describe( 'add to queue', function() {
 			before( function() {
 				this.procession = new Procession();
 			} );
@@ -64,6 +64,50 @@ define( [ 'buster', '../procession', 'when' ], function( buster, Procession, whe
 					return when( this.procession.queue( load ) )
 						.then( function() {
 							expect( when.isPromise( load.getCall( 0 ).args[ 1 ] ) ).toBeTrue();
+						} )
+						.yield();
+				} );
+			} );
+
+			describe( 'Unload', function() {
+				it( 'is called', function() {
+					var unload = this.spy();
+
+					this.procession.queue( null, unload );
+
+					return when( this.procession.queue() )
+						.then( function() {
+							expect( unload ).toHaveBeenCalledOnce();
+						} )
+						.yield();
+				} );
+
+				it( 'is called twice', function() {
+					var unload = this.spy();
+
+					this.procession.queue( null, unload );
+
+					return when( this.procession.queue( null, unload ) )
+						.then( function() {
+							expect( unload ).toHaveBeenCalledOnce();
+						} )
+						.yield( this.procession.queue() )
+						.then( function() {
+							expect( unload ).toHaveBeenCalledTwice();
+						} )
+						.yield();
+				} );
+
+				it( 'is called with options', function() {
+					var unload = this.spy(),
+						options_prev = { boo: 'far' },
+						options = { foo: 'bar' };
+
+					this.procession.queue( null, unload, options_prev );
+
+					return when( this.procession.queue( null, null, options ) )
+						.then( function() {
+							expect( unload ).toHaveBeenCalledWith( { foo: 'bar', prevous: options_prev } );
 						} )
 						.yield();
 				} );
